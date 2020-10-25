dbfe8fa3e1f9d15911e487cc0b01f5e1627dba4a
Ralph <bogdanfirefly@gmail.com>
2020-10-25 11:30:47 +0200

    all from windows

--- bot/core.py
+++ bot/core.py
@@ -1,18 +1,17 @@
-# from aiogram import Bot, Dispatcher
-# from aiogram.contrib.fsm_storage.memory import MemoryStorage
-# from aiogram.utils.executor import Executor
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils.executor import Executor
 from sqlalchemy import create_engine
 
 from bot.config import TELEGRAM_BOT_TOKEN
-from bot.config import USERNAME
-from bot.config import SERVER
+from bot.config import DB_USERNAME
+from bot.config import DB_HOST
 from bot.config import PORT
 from bot.config import DB_NAME
 from bot.config import PASSWORD
 
-# bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode='HTML')
-# dp = Dispatcher(bot=bot, storage=MemoryStorage())
-# executor = Executor(dp, skip_updates=True)
+bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode='HTML')
+dp = Dispatcher(bot=bot, storage=MemoryStorage())
+executor = Executor(dp, skip_updates=True)
 
-engine = create_engine(f'posrgresql://{USERNAME}:{PASSWORD}@{SERVER}:{PORT}/{DB_NAME}')
-print(engine)
+engine = create_engine(f'postgresql://{DB_USERNAME}:{PASSWORD}@{DB_HOST}:{PORT}/{DB_NAME}')
