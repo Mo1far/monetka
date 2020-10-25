b951e5d29345e5c27dad6d35879a83bd159d749b
Ralph <bogdanfirefly@gmail.com>
2020-10-25 10:38:20 +0200

    fix name var iables

--- bot/core.py
+++ bot/core.py
@@ -1,6 +1,6 @@
-from aiogram import Bot, Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils.executor import Executor
+# from aiogram import Bot, Dispatcher
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
+# from aiogram.utils.executor import Executor
 from sqlalchemy import create_engine
 
 from bot.config import TELEGRAM_BOT_TOKEN
@@ -10,9 +10,9 @@ from bot.config import PORT
 from bot.config import DB_NAME
 from bot.config import PASSWORD
 
-bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode='HTML')
-dp = Dispatcher(bot=bot, storage=MemoryStorage())
-executor = Executor(dp, skip_updates=True)
+# bot = Bot(token=TELEGRAM_BOT_TOKEN, parse_mode='HTML')
+# dp = Dispatcher(bot=bot, storage=MemoryStorage())
+# executor = Executor(dp, skip_updates=True)
 
 engine = create_engine(f'posrgresql://{USERNAME}:{PASSWORD}@{SERVER}:{PORT}/{DB_NAME}')
 print(engine)
